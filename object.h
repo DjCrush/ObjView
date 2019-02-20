5d82cf86312f512abc7698800300e7f48ce43b58
Yevgeny Antonov <yevgeny1978@mail.ru>
2019-02-21 00:37:49 +0700

    Update object.h

--- object.h
+++ object.h
@@ -9,50 +9,18 @@
 #include <iostream>
 #include <cstdlib>
 #include <SDL_timer.h>
-
-
-
 using namespace std;
 
-
 class Object {
-
 public:
-	
 	struct polygon
 	{
-		float x1;
-		float x2;
-		float x3;
-		float y1;
-		float y2;
-		float y3;
-		float z1;
-		float z2;
-		float z3;
-		float xn1;
-		float xn2;
-		float xn3;
-		float yn1;
-		float yn2;
-		float yn3;
-		float zn1;
-		float zn2;
-		float zn3;
-		polygon(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, float xn1, float yn1, float zn1, float xn2, float yn2, float zn2, float xn3, float yn3, float zn3)
-			: x1(x1), y1(y1), z1(z1), x2(x2), y2(y2), z2(z2), x3(x3), y3(y3), z3(z3), xn1(xn1), yn1(yn1), zn1(zn1), xn2(xn2), yn2(yn2), zn2(zn2), xn3(xn3), yn3(yn3), zn3(zn3)
-		{}
-
+		float x1, x2, x3, y1, y2, y3, z1, z2, z3;
+		float xn1, xn2, xn3, yn1, yn2, yn3, zn1, zn2, zn3;
 	};
 	struct point
 	{
-		float x;
-		float y;
-		float z;
-		point(float x, float y, float z)
-			: x(x), y(y), z(z)
-		{}
-
+		float x, y, z;
 	};
 	
 	Object(); 
@@ -60,14 +28,10 @@ public:
 	void DrawGuro(int x_c2, int y_c2, float type);
 	void Rotate(float AngleX);
 	void ReadObjectFromOBJ(const char* filename);
-	string GetToken(string my_string);
-
-
 private:
+	string GetToken(string my_string);
 	float LightX, LightY, LightZ;
 	int Dist;
 	size_t marker;
 	vector <polygon> faces;
 };
-
-
