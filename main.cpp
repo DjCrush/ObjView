5153314bb217f4a16065aa31f82c76fa09a4f2fc
Yevgeny Antonov <yevgeny1978@mail.ru>
2019-02-20 17:54:12 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,36 +3,22 @@
 #include <Windows.h>
 #include "mygraphicslib.h"
 #include "object.h"
-
-
-
-
 using namespace std;
-
 
 SDL_Surface *screen;
 SDL_Surface *font;
-
 
 int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 {
 	SDL_Event event;
 	int keypress = 0;
-	
-
-
-
-
-
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
-
-	
 	if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_HWSURFACE)))
 	{
 		SDL_Quit();
 		return 1;
 	}
-	
+
 	if (!(font = SDL_LoadBMP("font.bmp")))
 	{
 		
@@ -41,14 +27,12 @@ int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 		SDL_Quit();
 		return 1;
 	}
-
 	Object tor;
 	//tor.ReadObjectFromOBJ("E:/EXP/3dmax/obj/alfa147.obj"); float scale = 3;
 	tor.ReadObjectFromOBJ("E:/EXP/3dmax/obj/1.obj"); float scale = 300;
 	//tor.ReadObjectFromOBJ("mod.obj"); float scale = 3;
 	//tor.ReadObjectFromOBJ("E:/EXP/3dmax/obj/bmw.obj"); float scale = 0.5;
 	//tor.ReadObjectFromOBJ("E:/EXP/3dmax/obj/hand/hand.obj"); float scale = 200;
-
 	while (!keypress)
 	{
 		ClsScreen();
@@ -68,17 +52,9 @@ int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 			}
 		}
 	}
-
-	
-
-
 	SDL_FreeSurface(font);
 	SDL_FreeSurface(screen);
 	SDL_Quit();
-
-
 	delete Z_buffer;
-
 	return 0;
 }
-
