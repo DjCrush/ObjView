e1d9a3aba05b6f5bb2f6e830f9ab2cca6ac737c8
Yevgeny Antonov <yevgeny1978@mail.ru>
2019-06-21 08:28:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,40 @@
 #include <SDL.h>
-#include <cstdlib>
 #include <Windows.h>
-#include "mygraphicslib.h"
-#include "object.h"
+#include "graphicslib.h"
+#include "mesh.h"
 using namespace std;
 
 SDL_Surface *screen;
 SDL_Surface *font;
+SDL_Renderer *renderer;
+int *Z_buffer = new int[WIDTH * HEIGHT];
 
 int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 {
 	SDL_Event event;
+	SDL_Window *window = NULL;
 	int keypress = 0;
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
-	if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_HWSURFACE)))
-	{
-		SDL_Quit();
-		return 1;
-	}
 
+	window = SDL_CreateWindow(
+		"ObjViewer",                  // window title
+		SDL_WINDOWPOS_UNDEFINED,           // initial x position
+		SDL_WINDOWPOS_UNDEFINED,           // initial y position
+		WIDTH,                               // width, in pixels
+		HEIGHT,                               // height, in pixels
+		SDL_WINDOW_OPENGL                  // flags - see below
+	);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 	if (!(font = SDL_LoadBMP("font.bmp")))
 	{
-		
 		MessageBoxA(NULL, "File 'font.bmp' not found", "Error", MB_OK);
-
 		SDL_Quit();
 		return 1;
 	}
-	Object tor;
-	//tor.ReadObjectFromOBJ("E:/EXP/3dmax/obj/alfa147.obj"); float scale = 3;
-	tor.ReadObjectFromOBJ("E:/EXP/3dmax/obj/1.obj"); float scale = 300;
-	//tor.ReadObjectFromOBJ("mod.obj"); float scale = 3;
-	//tor.ReadObjectFromOBJ("E:/EXP/3dmax/obj/bmw.obj"); float scale = 0.5;
-	//tor.ReadObjectFromOBJ("E:/EXP/3dmax/obj/hand/hand.obj"); float scale = 200;
+	Mesh tor;
+	tor.Read("E:/EXP/3dmax/obj/alfa147.obj");
 	while (!keypress)
 	{
-		ClearScreen();
-	//	tor.Draw(WIDTH / 2, HEIGHT / 2, scale);
-		tor.DrawGuro(WIDTH / 2, HEIGHT / 2, scale);
-		tor.Rotate(0.5);
 		while (SDL_PollEvent(&event))
 		{
 			switch (event.type)
@@ -51,10 +47,18 @@ int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 				break;
 			}
 		}
+		SDL_RenderClear(renderer);
+		ClearScreen();
+		//tor.Draw(WIDTH / 2, HEIGHT / 2);
+		tor.DrawGuro(WIDTH / 2, HEIGHT / 2);
+		tor.Rotate(0.1);
+		SDL_RenderPresent(renderer);
 	}
+	delete[] Z_buffer;
 	SDL_FreeSurface(font);
 	SDL_FreeSurface(screen);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
-	delete[] Z_buffer;
 	return 0;
 }
